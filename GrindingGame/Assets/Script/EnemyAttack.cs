1cf99d748ef4fb8e01c68c6b6fe93e4a9699b9e2
MadsS <stoltenborg@hotmail.dk>
2018-04-17 11:44:50 +0200

    Lavet main menu scene og Rettet små ting

--- GrindingGame/Assets/Script/EnemyAttack.cs
+++ GrindingGame/Assets/Script/EnemyAttack.cs
@@ -9,26 +9,13 @@ public class EnemyAttack : MonoBehaviour
     GameObject _player;
     public Animation anim;
 
-    //Swrod collider evt
-    GameObject sword;
-    Collider sword_collider;
-
-    GameObject druidHand;
-    Collider hand_collider;
+ 
 
     void Awake()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
         _animator = GetComponent<Animator>();
         anim = GetComponent<Animation>();
-
-        sword = GameObject.FindGameObjectWithTag("AssassinWeapon");
-        sword_collider = sword.GetComponent<Collider>();
-        sword_collider.enabled = false;
-
-        druidHand = GameObject.FindGameObjectWithTag("DruideWeapon");
-        hand_collider = druidHand.GetComponent<Collider>();
-        hand_collider.enabled = false;
 
     }
 
@@ -53,12 +40,7 @@ public class EnemyAttack : MonoBehaviour
             _animator.SetBool("running", false);
          
             _animator.SetBool("IsNearPlayer", true);
-
-           if(!_animator.GetCurrentAnimatorStateInfo(0).IsName("Basic Attack") && other.gameObject.tag == ("Player"))
-            {
-                sword_collider.enabled = true;
-                hand_collider.enabled = true;
-            }
+   
         }
     }
 
